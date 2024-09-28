ae63123efda26eba8a5c20ba619979606c086ad3
luis-oliv <luisoliveira.me@gmail.com>
2024-09-27 21:03:23 -0300

    refatoração fetchAnimais

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ modal.init()
 const tooltip = new ToolTip('[data-tooltip]')
 tooltip.init()
 
-FetchAnimais('../animaisAPI.json', '.numeros-grid')
+FetchAnimais('../../animaisapi.json', '.numeros-grid')
 
 
 
